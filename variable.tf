943ed440a061adc53811f9d782a14e2b64dd184d
expertjaved <36668943+expertjaved@users.noreply.github.com>
2024-01-07 02:16:37 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -6,3 +6,9 @@ output "printfirst" {
   value = "${join("," ,var.users)}" # Accessing the first element of the users list
 }
 
+output helloworldupper{
+   value = "${upper(var.users[0])}"
+}
+
+output hellowordlower {
+   value = "${lower(var.users[1])}"
