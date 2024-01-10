68fd332fe09b65f880aa48d134572ba6fac1b185
expertjaved <36668943+expertjaved@users.noreply.github.com>
2024-01-10 20:58:03 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0d3f444bc76de0a79"
+  instance_type = "t2.micro"
+
+  count = 2
+
+  tags = {
+    Name = "example-instance"
+  }
+}
